0a446dd05621d48aae01482c59db6a4744527923
Hassan Al-khalifah and William Wittenbrock <william@>
2018-08-13 16:49:51 -0700

    Finish project objectives.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var prices = [5,2.53,7.5,2,4.75,7.25];
+var prices = [5,2.5,7.5,2,4.75,7.25];
 var total = 0;
 var groceryList = ["item1", "item2", "item3", "item4", "item5", "item6"];
 var grocery;
@@ -6,6 +6,7 @@ var grocery;
 $(document).ready(function() {
   $("#form").submit(function(event){
     event.preventDefault();
+
     prices.forEach(function(price, index) {
       grocery = $("#" + groceryList[index]).val();
       total += price * parseFloat(grocery);
