31043afe89b93040ea12f04bb5c5cf18c9f8c65c
Hassan Al-khalifah and William Wittenbrock <william@>
2018-08-13 16:18:31 -0700

    Add working input and totalling javascipt functions for the form, add css styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
+var prices = [5,2.5,7.5,2,4.75,7.25];
+var total = 0;
+var groceryList = ["item1", "item2", "item3", "item4", "item5", "item6"];
+var grocery;
+
+
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+
+  $("#form").submit(function(event){
     event.preventDefault();
 
+
+
+    prices.forEach(function(price, index) {
+
+      grocery = $("#" + groceryList[index]).val();
+      total += price * parseInt(grocery);
+
+
+    });
+
+
+
+
+
+
+$("#totalPrice").append("$" + total);
   });
 });
