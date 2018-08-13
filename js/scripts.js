a9fb6a898b4b0a87b31e661ef80bd93ffe6f7cef
Hassan Al-khalifah and William Wittenbrock <william@>
2018-08-13 16:29:43 -0700

    style title font size, change oprice output class to id, clean up index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,15 @@
-var prices = [5,2.5,7.5,2,4.75,7.25];
+var prices = [5,2.53,7.5,2,4.75,7.25];
 var total = 0;
 var groceryList = ["item1", "item2", "item3", "item4", "item5", "item6"];
 var grocery;
 
-
 $(document).ready(function() {
-
   $("#form").submit(function(event){
     event.preventDefault();
-
-
-
     prices.forEach(function(price, index) {
-
       grocery = $("#" + groceryList[index]).val();
-      total += price * parseInt(grocery);
-
-
+      total += price * parseFloat(grocery);
     });
-
-
-
-
-
-
 $("#totalPrice").append("$" + total);
   });
 });
